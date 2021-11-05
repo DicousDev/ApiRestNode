19e429efc0ba79dab950adb00a5bd268eee906be
João Victor Duarte <joaodicu387@gmail.com>
2021-11-05 14:53:44 -0300

    Criando rota para adicionar Person ao MongoDB

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 5000;
 
+const Person = require("./models/Person");
+
 app.use(
     express.urlencoded({
         extended: true
@@ -13,6 +15,30 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({ message: "Express" });
+});
+
+app.post("/person", async (req, res) => {
+    const {name, age, salary, approved} = req.body;
+
+    if(!name || !age || !salary || !approved) {
+        res.status(422).json({error: `Todos os campos são obrigatórios`})
+        return false;
+    }
+
+    const person = {
+        name,
+        age,
+        salary,
+        approved
+    };
+
+    try {
+        await Person.create(person);
+        res.status(201).json({message: "Pessoa inserida no sistema com sucesso."});
+    }
+    catch(error) {
+        res.status(500).json({error: error});
+    }
 });
 
 const DB_USER = "duarte"
