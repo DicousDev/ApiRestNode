e18e44373b40511e87d0c6ea08dd8f4356ac1849
João Victor Duarte <joaodicu387@gmail.com>
2021-11-05 15:30:13 -0300

    Criando rota para obter uma pessoa dado o ID

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -11,6 +11,24 @@ router.get("/", async (req, res) => {
     } 
 });
 
+router.get("/:id", async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const person = await Person.findOne({_id: id});
+
+        if(!person) {
+            res.status(422).json({message: "O usuário não foi encontrado"});
+            return false;
+        }
+
+        res.status(200).json(person);
+    }
+    catch (error) {
+        res.status(500).json({error: error});
+    }
+});
+
 router.post("/", async (req, res) => {
     const {name, age, salary, approved} = req.body;
 
