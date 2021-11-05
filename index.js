0eacbf0e3ddafc95be34c60859211be5ab03836a
João Victor Duarte <joaodicu387@gmail.com>
2021-11-05 14:27:52 -0300

    Fazendo conexão ao MongoDB

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 5000;
 
@@ -14,6 +15,19 @@ app.get("/", (req, res) => {
     res.json({ message: "Express" });
 });
 
-app.listen(port, () => {
-    console.log(`Rodando na porta ${port} 🚀`);
+const DB_USER = "duarte"
+const DB_PASSWORD = encodeURIComponent("LPXSi0A8hIYcgDD4");
+
+// LPXSi0A8hIYcgDD4
+// mongodb+srv://duarte:LPXSi0A8hIYcgDD4@apicluster.mm7xl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.mm7xl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+.then(() => {
+    console.log("Conectamos ao MongoDB");
+    app.listen(port, () => {
+        console.log(`Rodando na porta ${port} 🚀`);
+    });
+})
+.catch((error) => {
+    console.log(error);
 });
